5c43d3c9cce751225b0874f4a40c508a1b1c483a
Harry Field <59520303+hpfield@users.noreply.github.com>
2024-02-12 10:06:13 +0000

    Finishing up predictions

--- src/train_predict.py
+++ src/train_predict.py
@@ -5,6 +5,13 @@ from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
+# ada boost regressor
+from sklearn.ensemble import AdaBoostRegressor
+# gradient boost regressor
+from sklearn.ensemble import GradientBoostingRegressor
+# xgboost regressor
+from xgboost import XGBRegressor
+import time
 
 # Load datasets
 train_df = pd.read_csv('data/train_cleaned.csv')
@@ -21,23 +28,30 @@ test_df.drop(columns=['Id'], inplace=True)
 X = train_df.drop(columns=['SalePrice'])
 y = train_df['SalePrice']
 
+# Standardise the data
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+test_df = scaler.transform(test_df)
+
 # Define models to try
 models = {
     'LinearRegression': LinearRegression(),
     'Ridge': Ridge(),
     'Lasso': Lasso(),
-    'RandomForestRegressor': RandomForestRegressor()
+    'RandomForestRegressor': RandomForestRegressor(),
+    "ada_boost": AdaBoostRegressor(),
+    "gradient_boost": GradientBoostingRegressor(),
+    "xgboost": XGBRegressor()
 }
 
 # Fit models and make predictions on the test set
 for name, model in models.items():
-    pipeline = make_pipeline(StandardScaler(), model)
-    pipeline.fit(X, y)  # Fit on the entire training dataset
-    predictions = pipeline.predict(test_df)
+    model.fit(X, y)
+    predictions = model.predict(test_df)
 
     # Save predictions to CSV
     output_df = pd.DataFrame({'Id': test_ids, 'SalePrice': predictions})
-    output_file_name = f'../submissions/predictions_{name}.csv'
+    output_file_name = f"../submissions/predictions_{name}_{time.strftime('%Y_%m_%d_%H:%M:%S')}.csv"
     output_df.to_csv(output_file_name, index=False)
     print(f'Predictions saved to {output_file_name}')
 
